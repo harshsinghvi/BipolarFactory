7ff7878c6916b892448c2b87c196f9dee5a960d6
Rahul Raj <rahulraz1308@gmail.com>
2021-02-23 18:27:32 +0530

    lost changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
